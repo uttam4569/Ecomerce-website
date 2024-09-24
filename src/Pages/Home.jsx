7357ada7d25fe9bae22b00ded7a3df67f7b839b6
Uttam Kumar <132780812+uttam4569@users.noreply.github.com>
2024-09-25 00:40:49 +0530

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const HomePage = () => {
   return (
     <>
-     <img src="https://plus.unsplash.com/premium_photo-1683984171269-04c84ee23234?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8ZWNvbW1lcmNlfGVufDB8fDB8fHww"></img>
+     <img src="https://plus.unsplash.com/premium_photo-1683984171269-04c84ee23234?q=80&w=2874&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"></img>
     </>
   );
 };
